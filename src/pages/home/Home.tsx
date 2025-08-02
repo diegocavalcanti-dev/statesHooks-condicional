9ab6c591efbeb198cfcee995d7ae04e1e614fb68
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-08-01 23:03:11 -0300

    feat: contexts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,143 +1,25 @@
-﻿// interface homeProps {
-//     titulo: string;
-//     texto: string;
-// }
+﻿import { useContext } from 'react';
 
-function Home(
-    // props: homeProps
-) {
-    return (
-        <div>
-            <div className="grid grid-cols-12 gap-2 m-4">
-                <div className="col-span-6 bg-green-200">
-                    container 1
-                </div> 
+import { Link } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
 
-                <div className="col-span-6 bg-yellow-200">
-                    container 2
-                </div>
+function Home() {
 
-            </div>
+  const { nome } = useContext(UserContext);
 
-            <div className="grid grid-cols-12 gap-2 m-4">
-                <div className="col-span-8 bg-green-200">
-                    container 1
-                </div> 
-
-                <div className="col-span-4 bg-yellow-200">
-                    container 2
-                </div> 
-            </div>
-
-            
-            <div className="grid grid-cols-12 gap-2 m-4">
-                <div className="col-span-4 bg-green-200">
-                    container 1
-                </div> 
-
-                <div className="col-span-4 bg-yellow-200">
-                    container 2
-                </div> 
-
-                <div className="col-span-4 bg-red-200">
-                    container 3
-                </div> 
-            </div>
-
-            <div className="grid grid-cols-12 gap-2 m-4">
-                <div className="col-span-3 bg-green-200">
-                    container 1
-                </div> 
-
-                <div className="col-span-3 bg-yellow-200">
-                    container 2
-                </div> 
-
-                <div className="col-span-3 bg-red-200">
-                    container 3
-                </div> 
-
-                <div className="col-span-3 bg-blue-200">
-                    container 4
-                </div> 
-            </div>
-
-            <div className="grid grid-cols-12 gap-2 m-4">
-                <div className="col-span-4 bg-green-200">
-                    container 1
-                </div> 
-
-                <div className="col-span-4 bg-yellow-200">
-                    container 2
-                </div> 
-
-                <div className="col-span-2 bg-red-200">
-                    container 3
-                </div> 
-
-                <div className="col-span-2 bg-blue-200">
-                    container 4
-                </div> 
-            </div>
-
-            <div className="hidden">
-                <div>
-                <h3 className="text-xl">justify content alinhamento horizontal</h3>
-                <h5>justify-start Esquerda</h5>
-                <div className="flex flex-row justify-start m-3">
-                    <div className="basis-1/4 bg-red-500">01</div>
-                    <div className="basis-1/4 bg-gray-400">01</div>
-                </div>
-
-                <h5>justify-end Direita</h5>
-                <div className="flex flex-row justify-end m-3">
-                    <div className="basis-1/4 bg-red-500">01</div>
-                    <div className="basis-1/4 bg-gray-400">01</div>
-                </div>
-
-                <h5>justify-center Centro</h5>
-                <div className="flex flex-row justify-center m-3">
-                    <div className="basis-1/4 bg-red-500">01</div>
-                    <div className="basis-1/4 bg-gray-400">01</div>
-                </div>
-
-                <h5>justify-around Extremidades</h5>
-                <div className="flex flex-row justify-between m-3">
-                    <div className="basis-1/4 bg-red-500">01</div>
-                    <div className="basis-1/4 bg-gray-400">01</div>
-                </div>
-
-                <h5>justify-around estremidades com espaçamento</h5>
-                <div className="flex flex-row justify-around m-3">
-                    <div className="basis-1/4 bg-red-500">01</div>
-                    <div className="basis-1/4 bg-gray-400">01</div>
-                </div>
-
-            </div>
-
-            <div className="mt-12">
-                <h3 className="text-xl">justify content alinhamento vertical</h3>
-
-                <h3 className="text-xl">justify-content alinhamento vertical</h3>
-                    
-                    <div className="w-auto h-60 bg-gray-400 flex justify-center items-start m-3">
-                        <div className="basis-1/4 bg-red-500">justify-center items-start alinhamento horizontal</div>
-                    </div>
-
-                    <div className="w-auto h-60 bg-gray-400 flex justify-center items-center m-3">
-                        <div className="basis-1/4 bg-red-500">justify-center items-start alinhamento horizontal</div>
-                    </div>
-
-                    <div className="w-auto h-60 bg-gray-400 flex justify-center items-end m-3">
-                        <div className="basis-1/4 bg-red-500">justify-center items-start alinhamento horizontal</div>
-                    </div>
-
-            </div>
-            </div>
-            {/* <h2>{props.titulo}</h2>
-            <p>{props.texto}</p> */}
-        </div>
-    );
+  return (
+    <div className='container'>
+      <div>
+        <h1>Autenticar Usuário</h1>
+        <h2>Seja Bem Vinde: <span>{nome}</span></h2>
+        <Link to="/login" 
+            className="botao"
+        >
+          Voltar
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
